466f8b85e26b534410071264ec078424728dfed1
Deni Himawan <denihimawan595@gmail.com>
2025-11-19 16:49:24 +0700

    buat fungsi get by id dan validasi id

---
+++ api/src/app/api/foundreport/[slug]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+
+    // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
+}
