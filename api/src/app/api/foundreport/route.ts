32c72bec36e3be4bf7f3dad026eb3aab7f208d4d
Deni Himawan <denihimawan595@gmail.com>
2025-11-19 16:45:33 +0700

    buat response succes pada fungsi create di post found

--- api/src/app/api/foundreport/route.ts
+++ api/src/app/api/foundreport/route.ts
@@ -156,4 +156,13 @@ export async function POST(req: Request) {
         }
       }
     });
+    // Response Success
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Laporan barang temuan berhasil dibuat",
+        data: report
+      },
+      { status: 201 }
+    );
 }
