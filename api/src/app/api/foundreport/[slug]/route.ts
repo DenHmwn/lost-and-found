a95adcdbd8e4642d7278f91c606fe061e328f696
Deni Himawan <denihimawan595@gmail.com>
2025-11-19 17:46:24 +0700

    buat fungsi delete by id di found

--- api/src/app/api/foundreport/[slug]/route.ts
+++ api/src/app/api/foundreport/[slug]/route.ts
@@ -253,3 +253,22 @@ export async function PUT(
     );
   }
 }
+
+// DELETE found report
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ slug: string }> }
+) {
+    const { slug } = await params;
+    // Validasi ID
+    const id = Number(slug);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "ID tidak valid",
+        },
+        { status: 400 }
+      );
+    }
+}
