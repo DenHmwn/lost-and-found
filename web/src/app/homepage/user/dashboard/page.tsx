c626dcc60607421084d63d0083b2b96b2354e870
Deni Himawan <denihimawan595@gmail.com>
2025-11-21 15:09:21 +0700

    update dashboard user

--- web/src/app/homepage/user/dashboard/page.tsx
+++ web/src/app/homepage/user/dashboard/page.tsx
@@ -10,7 +10,7 @@ import {
 
 import data from "./data.json"
 
-export default function DashboardUser() {
+export default function Page() {
   return (
     <SidebarProvider
       style={
@@ -28,7 +28,7 @@ export default function DashboardUser() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                {/* <ChartAreaInteractive /> */}
               </div>
               <DataTable data={data} />
             </div>
