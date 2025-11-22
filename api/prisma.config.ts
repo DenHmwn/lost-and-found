cb3890929bb4cc5ad28733c5b2258ad877f4dad9
Deni Himawan <denihimawan595@gmail.com>
2025-11-22 13:44:22 +0700

    update url db env di schema

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig, env } from "prisma/config";
-import "dotenv/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
-
-
-// import { defineConfig } from "@prisma/config";
+// // import { defineConfig, env } from "prisma/config";
+// import "dotenv/config";
+// import { env } from "process";
 
 // export default defineConfig({
-//   schema: "./prisma/schema.prisma",
+//   schema: "prisma/schema.prisma",
 //   datasource: {
-//     url: process.env.DATABASE_URL!,
+//     url: env.DATABASE_URL,
 //   },
 // });
+
+
+// // // import { defineConfig } from "@prisma/config";
+
+// // // export default defineConfig({
+// // //   schema: "./prisma/schema.prisma",
+// // //   datasource: {
+// // //     url: process.env.DATABASE_URL!,
+// // //   },
+// // // });
