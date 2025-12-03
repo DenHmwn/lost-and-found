4b54089d03d160fd9b1fd480369e03ec13c90c8a
Deni Himawan <denihimawan595@gmail.com>
2025-12-03 15:20:30 +0700

    update migrasi dan matcher

--- api/prisma.config.ts
+++ api/prisma.config.ts
@@ -18,3 +18,13 @@
 // // //     url: process.env.DATABASE_URL!,
 // // //   },
 // // // });
+
+import { defineConfig } from "prisma/config";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
