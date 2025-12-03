62bc2b79c21b1755ff54795e09c0551e840ecb2f
Deni Himawan <denihimawan595@gmail.com>
2025-12-03 14:22:11 +0700

    set middleware

--- api/src/middleware.ts
+++ api/src/middleware.ts
@@ -1,8 +1,25 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware() {
+export function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
   const response = NextResponse.next();
+
+  // skip nextjs internal paths
+  if (pathname.startsWith("/_next/")) {
+    return NextResponse.next();
+  }
+
+  // kasih access auth routes
+  if (  
+    pathname.startsWith("/api/auth/login") ||
+    pathname.startsWith("/api/auth/register") ||
+    pathname.startsWith("/api/auth/verify")
+  ) {
+    return NextResponse.next();
+  }
+
   response.headers.set("Access-Control-Allow-Origin", "*");
   response.headers.set(
     "Access-Control-Allow-Methods",
