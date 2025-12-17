0ffce1bc985b75e84de92ecc53f489d9c895f353
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2025-12-17 19:18:43 +0700

    Add: loading spinner

--- web/src/components/admin/DashboardAdmin.tsx
+++ web/src/components/admin/DashboardAdmin.tsx
@@ -154,7 +154,7 @@ export default function DashboardAdmin() {
       return (
       <div className="min-h-screen bg-gray-50 flex items-center justify-center">
         <div className="text-center">
-        
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-500 mx-auto mb-4"></div>
         );
 
 
