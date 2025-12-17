07d96300c50a6f65541239d34cb1f8b921afd2b7
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2025-12-17 18:32:55 +0700

    Initial file setup and imports

---
+++ web/src/components/admin/DashboardAdmin.tsx
@@ -0,0 +1,5 @@
+'use client';
+import { AppSidebarAdmin } from "@/components/AppSidebarAdmin";
+import { SiteHeader } from "@/components/SiteHeader";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
