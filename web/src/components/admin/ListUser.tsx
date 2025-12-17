810acf7be2ff39547ce45d883c64c2622c26d41d
Deni Himawan <denihimawan595@gmail.com>
2025-12-17 18:12:38 +0700

    feat: update list user

--- web/src/components/admin/ListUser.tsx
+++ web/src/components/admin/ListUser.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { AppSidebarAdmin } from '../AppSidebarAdmin';
+import { SidebarInset, SidebarProvider } from '../ui/sidebar';
+import { SiteHeader } from '../SiteHeader';
 
 export default function ListUser() {
   return (
-    <div>
-      List User
-    </div>
-  )
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebarAdmin variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <section className="flex flex-1 flex-col">
+          <section className="@container/main flex flex-1 flex-col gap-6 p-6">
+          List User
+          </section>
+        </section>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
