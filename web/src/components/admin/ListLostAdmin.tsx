d458cf8ac8ffea10f70ec35045349da6e6d393c3
Deni Himawan <denihimawan595@gmail.com>
2025-12-18 20:32:19 +0700

    refactor: change name

--- web/src/components/admin/ListLostAdmin.tsx
+++ web/src/components/admin/ListLostAdmin.tsx
@@ -337,7 +337,7 @@ export default function ListLostAdmin() {
                             Status Laporan
                           </TableHead>
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
-                            Status Laporan
+                            Kondisi Laporan
                           </TableHead>
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Tanggal
