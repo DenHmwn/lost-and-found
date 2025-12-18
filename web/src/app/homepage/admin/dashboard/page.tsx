3dcd37009027e920de3b2bad830c467c43ed1e1c
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2025-12-18 12:08:52 +0700

    fix: DashboardAdmin

--- web/src/app/homepage/admin/dashboard/page.tsx
+++ web/src/app/homepage/admin/dashboard/page.tsx
@@ -1,24 +1,6 @@
-import { AppSidebarAdmin } from "@/components/AppSidebarAdmin";
-import { SiteHeader } from "@/components/SiteHeader";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-
-export default function DashboardAdmin() {
+import DashboardAdmin from "@/components/admin/DashboardAdmin";
+export default function DashboardAdminPage() {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebarAdmin variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <section className="flex flex-1 flex-col">
-          <section className="@container/main flex flex-1 flex-col gap-2"></section>
-        </section>
-      </SidebarInset>
-    </SidebarProvider>
+    <DashboardAdmin/>
   );
 }
