cfb6ed9b27d77a3e64544c5b11bfc0d29358a6ae
Deni Himawan <denihimawan595@gmail.com>
2025-12-24 19:11:47 +0700

    feat: add recent skeleton dashboard

--- web/src/components/SkeletonDashboard.tsx
+++ web/src/components/SkeletonDashboard.tsx
@@ -32,6 +32,36 @@ export default function SkeletonDasboard() {
           </section>
         ))}
       </section>
+
+        {/* bagian kiri */}
+        <section className="col-span-1 lg:col-span-4 space-y-4">
+          <section className="rounded-xl border bg-card shadow-sm">
+            <section className="p-6">
+              <Skeleton className="h-6 w-[150px] mb-6" />
+              {/* Aktivitas Terbaru */}
+              {/* List Items Skeleton */}
+              <section className="space-y-8">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <section key={i} className="flex items-center">
+                    <Skeleton className="h-9 w-9 rounded-full mr-4" />
+                    {/* Icon Avatar/Barang */}
+                    <section className="space-y-1 flex-1">
+                      <Skeleton className="h-4 w-[200px]" />
+                      <section className="flex gap-2">
+                        <Skeleton className="h-3 w-[80px]" />
+                        {/* Badge Status */}
+                        <Skeleton className="h-3 w-[60px]" />
+                        {/* Badge Pending */}
+                      </section>
+                    </section>
+                    <Skeleton className="h-3 w-[70px]" /> {/* bagian recent */}
+                  </section>
+                ))}
+              </section>
+            </section>
+          </section>
+        </section>
+
     </section>
   );
 }
