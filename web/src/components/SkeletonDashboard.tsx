c64b82092e623e7a1bcad317155301b42ff81e09
Deni Himawan <denihimawan595@gmail.com>
2025-12-24 19:05:47 +0700

    feat: add skeleton card dasboard

--- web/src/components/SkeletonDashboard.tsx
+++ web/src/components/SkeletonDashboard.tsx
@@ -11,6 +11,27 @@ export default function SkeletonDasboard() {
         </section>
         <Skeleton className="h-4 w-[180px]" /> {/* Tanggal */}
       </section>
+
+      {/* --- Stats 4 Cards  --- */}
+      <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <section
+            key={i}
+            className="rounded-xl border bg-card text-card-foreground shadow-sm p-6 space-y-4"
+          >
+            <section className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <Skeleton className="h-10 w-10 rounded-full" /> {/* Icon Box */}
+              <Skeleton className="h-4 w-4" /> {/* Icon Panah Kecil */}
+            </section>
+            {/* isi dari 4 card */}
+            <section className="space-y-2">
+              <Skeleton className="h-4 w-[100px]" />
+              <Skeleton className="h-8 w-[60px]" />
+              <Skeleton className="h-3 w-[80px]" />
+            </section>
+          </section>
+        ))}
+      </section>
     </section>
   );
 }
