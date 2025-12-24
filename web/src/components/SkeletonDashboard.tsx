83212d43acb61c23391ab60a889f67195f64a240
Deni Himawan <denihimawan595@gmail.com>
2025-12-24 19:04:40 +0700

    feat: add header skeleton dashboard

--- web/src/components/SkeletonDashboard.tsx
+++ web/src/components/SkeletonDashboard.tsx
@@ -1,4 +1,16 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function SkeletonDasboard() {
   return (
+    <section className="flex-1 space-y-6 p-8 pt-6">
+      {/* bagian header*/}
+      <section className="flex items-center justify-between space-y-2">
+        <section className="space-y-2">
+          <Skeleton className="h-8 w-[200px]" /> {/* Judul Dashboard */}
+          <Skeleton className="h-4 w-[150px]" /> {/* Subtext */}
+        </section>
+        <Skeleton className="h-4 w-[180px]" /> {/* Tanggal */}
+      </section>
+    </section>
   );
 }
