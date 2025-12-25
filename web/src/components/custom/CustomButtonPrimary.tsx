23c33e2eb6ec3e1aaa00088226a74243b6f968e9
Deni Himawan <denihimawan595@gmail.com>
2025-12-25 16:31:57 +0700

    docs: minor update

--- web/src/components/custom/CustomButtonPrimary.tsx
+++ web/src/components/custom/CustomButtonPrimary.tsx
@@ -10,7 +10,6 @@ export function CustomButtonPrimary({ label, onClick, className = "" }: CustomBu
   return (
     <Button
       onClick={onClick}
-      // Kita gabungkan class default dengan className dari props
       className={`bg-gray-900 text-white hover:bg-gray-800 transition-all ${className}`}
     >
       {label}
@@ -22,8 +21,8 @@ export function CustomButtonOutline({ label, onClick, className = "" }: CustomBu
   return (
     <Button
       onClick={onClick}
-      variant="outline" // Pakai variant outline dari shadcn
-      className={`bg-white text-gray-900 border-gray-300 hover:bg-gray-200 transition-all ${className}`}
+      variant="outline"
+      className={`bg-white text-gray-900 border-gray-300 hover:bg-gray-50 transition-all ${className}`}
     >
       {label}
     </Button>
