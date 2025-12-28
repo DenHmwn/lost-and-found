3854247c5421540a758ccb18f1de9b7c50d2a9cb
Deni Himawan <denihimawan595@gmail.com>
2025-12-28 09:42:45 +0700

    feat: add sidebar skeleton list member

---
+++ web/src/components/SkeletonListMember.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SkeletonMember() {
+   return (
+    <section className="min-h-screen bg-gray-50">
+      {/* Sidebar */}
+      <section className="fixed left-0 top-0 h-full w-64 bg-white border-r border-gray-200 p-4">
+        <section className="flex items-center gap-2 mb-8">
+          <section className="w-8 h-8 bg-gray-200 rounded" />
+          <section className="w-32 h-6 bg-gray-200 rounded" />
+        </section>
+        
+    </section>
+  );
+}
