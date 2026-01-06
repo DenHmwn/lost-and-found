5e0da7c92e60d88755db34838b37c00dffee874d
Deni Himawan <denihimawan595@gmail.com>
2026-01-06 15:49:57 +0700

    feat: add regex date report

--- web/src/lib/scripts.ts
+++ web/src/lib/scripts.ts
@@ -8,6 +8,21 @@ export const formatDate = (dateString: string) => {
     minute: "2-digit",
   });
 };
+export const formatDateReport = (date: Date) => {
+  const dd = pad(date.getDate());
+  const mm = pad(date.getMonth() + 1);
+  const yy = date.getFullYear().toString().slice(-2);
+
+  return `${dd}/${mm}/${yy}`;
+};
+
+const pad = (n: number) => n.toString().padStart(2, "0");
+export const createDate = (date: Date) => {
+  if (!date) return "";
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(
+    date.getDate()
+  )}`;
+};
 
 export const filterNotelp = (value: string) => {
   return value.replace(/[^0-9]/g, "");
