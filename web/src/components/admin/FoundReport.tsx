e8ad89d15dfd5df15e23e12c1875188b74b0f930
Dika Ramadhani <122988951+DikaRamadhani@users.noreply.github.com>
2026-01-07 22:13:59 +0700

    Update type deskripsi

--- web/src/components/admin/FoundReport.tsx
+++ web/src/components/admin/FoundReport.tsx
@@ -40,11 +40,11 @@ export default function LaporanBarangTemu() {
     setIsSubmitting(true);
     try {
       const payload = {
-        deskripsi,
         tanggal: date.toISOString().split("T")[0],
         waktu: time,
         namaBarang: namaBarang.trim(),
         lokasiTemu: lokasiTemu.trim(),
+        deskripsi : deskripsi.trim(),
       };
 
       const res = await api.post("/foundreport", payload);
