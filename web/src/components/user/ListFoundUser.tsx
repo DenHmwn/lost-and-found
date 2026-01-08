960aa358923dfda84f008f4b97699486038767cc
Deni Himawan <denihimawan595@gmail.com>
2026-01-09 01:46:50 +0700

    refactor: remove title admin in table found user

--- web/src/components/user/ListFoundUser.tsx
+++ web/src/components/user/ListFoundUser.tsx
@@ -210,9 +210,9 @@ export default function ListFoundUser() {
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Lokasi Temu
                           </TableHead>
-                          <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
+                          {/* <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Admin/Pelapor
-                          </TableHead>
+                          </TableHead> */}
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Status
                           </TableHead>
