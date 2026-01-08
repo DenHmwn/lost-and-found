080d6cec5432dcd727cf51f627649993eb248392
Deni Himawan <denihimawan595@gmail.com>
2026-01-09 01:48:43 +0700

    refactor: remove date report in table found user

--- web/src/components/user/ListFoundUser.tsx
+++ web/src/components/user/ListFoundUser.tsx
@@ -219,9 +219,9 @@ export default function ListFoundUser() {
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Tanggal Penemuan
                           </TableHead>
-                          <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
+                          {/* <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">
                             Tanggal Laporan
-                          </TableHead>
+                          </TableHead> */}
                         </TableRow>
                       </TableHeader>
                       <TableBody className="sectionide-y sectionide-border">
