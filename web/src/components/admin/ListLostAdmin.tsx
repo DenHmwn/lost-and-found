c848a28c0ac105c83a5e997f8d4a0e6a0668959f
Deni Himawan <denihimawan595@gmail.com>
2026-01-09 00:30:33 +0700

    feat: add title id in table lost admin

--- web/src/components/admin/ListLostAdmin.tsx
+++ web/src/components/admin/ListLostAdmin.tsx
@@ -252,6 +252,7 @@ export default function ListLostAdmin() {
                     <Table className="w-full">
                       <TableHeader>
                         <TableRow className="border-b bg-muted/30">
+                          <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">Id Barang</TableHead>
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">Info Barang</TableHead>
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">Lokasi</TableHead>
                           <TableHead className="px-6 py-4 text-left text-xs font-medium uppercase tracking-wider text-muted-foreground">Pelapor</TableHead>
