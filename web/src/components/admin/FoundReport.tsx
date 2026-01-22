bda40bed8daca038b1bb1fbd3fdd189efc7cc583
Deni Himawan <denihimawan595@gmail.com>
2026-01-22 22:04:52 +0700

    style: format code

--- web/src/components/admin/FoundReport.tsx
+++ web/src/components/admin/FoundReport.tsx
@@ -3,16 +3,29 @@ import { AppSidebarAdmin } from "@/components/AppSidebarAdmin";
 import { SiteHeader } from "@/components/SiteHeader";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Textarea } from "@/components/ui/textarea";
 import { api } from "@/lib/axios";
 import { ChevronDownIcon, PackageSearchIcon } from "lucide-react";
 import React from "react";
-import { CustomButtonOutline, CustomButtonPrimary } from "../custom/CustomButtonPrimary";
+import {
+  CustomButtonOutline,
+  CustomButtonPrimary,
+} from "../custom/CustomButtonPrimary";
 import { useRouter } from "next/navigation";
 import { createDate } from "@/lib/scripts";
 
@@ -33,7 +46,13 @@ export default function LaporanBarangTemu() {
   };
 
   const handleSubmit = async () => {
-    if (!namaBarang.trim() || !lokasiTemu.trim() || !deskripsi.trim() || !date || !time) {
+    if (
+      !namaBarang.trim() ||
+      !lokasiTemu.trim() ||
+      !deskripsi.trim() ||
+      !date ||
+      !time
+    ) {
       alert("Harap lengkapi semua data sebelum mengirim laporan.");
       return;
     }
@@ -43,8 +62,8 @@ export default function LaporanBarangTemu() {
       const payload = {
         namaBarang: namaBarang.trim(),
         lokasiTemu: lokasiTemu.trim(),
-        deskripsi : deskripsi.trim(),
-        tanggalTemu:createDate(date!),
+        deskripsi: deskripsi.trim(),
+        tanggalTemu: createDate(date!),
         waktuTemu: time,
       };
 
@@ -58,7 +77,10 @@ export default function LaporanBarangTemu() {
       setDate(undefined);
       setTime("");
     } catch (error: unknown) {
-      const message = error && typeof error === "object" && "response" in error ? "Gagal mengirim laporan: periksa kembali data atau login anda." : "Terjadi kesalahan saat mengirim laporan.";
+      const message =
+        error && typeof error === "object" && "response" in error
+          ? "Gagal mengirim laporan: periksa kembali data atau login anda."
+          : "Terjadi kesalahan saat mengirim laporan.";
       // console.error("Error:", error);
       alert(message);
     } finally {
@@ -87,8 +109,13 @@ export default function LaporanBarangTemu() {
                   <PackageSearchIcon className="h-5 w-5 text-primary" />
                 </section>
                 <section>
-                  <h1 className="text-2xl font-bold tracking-tight">Buat Laporan Barang Temu</h1>
-                  <p className="text-sm text-muted-foreground">Buat Laopran Untuk Barang Yang Anda Temui Agar Dapat Masuk Ke Dalam List Barang Temu</p>
+                  <h1 className="text-2xl font-bold tracking-tight">
+                    Buat Laporan Barang Temu
+                  </h1>
+                  <p className="text-sm text-muted-foreground">
+                    Buat Laopran Untuk Barang Yang Anda Temui Agar Dapat Masuk
+                    Ke Dalam List Barang Temu
+                  </p>
                 </section>
               </section>
             </header>
@@ -96,31 +123,67 @@ export default function LaporanBarangTemu() {
             {/* form laporan */}
             <Card>
               <CardHeader>
-                <CardTitle className="text-center text-2xl font-bold">Laporan Penemuan</CardTitle>
-                <CardDescription className="text-center">Masukkan Data Barang Anda Disini</CardDescription>
+                <CardTitle className="text-center text-2xl font-bold">
+                  Laporan Penemuan
+                </CardTitle>
+                <CardDescription className="text-center">
+                  Masukkan Data Barang Anda Disini
+                </CardDescription>
               </CardHeader>
               <CardContent>
                 <Label className="mx-2 mb-1.5 text-base">Nama Barang</Label>
-                <Input value={namaBarang} onChange={(e) => setNamaBarang(e.target.value)} maxLength={20} placeholder="Masukkan Nama Barang" className="mb-5"></Input>
-                <Label className="mx-2 mb-1.5 text-base ">Lokasi Barang Ditemukan</Label>
-                <Input value={lokasiTemu} onChange={(e) => setLokasiTemu(e.target.value)} maxLength={50} placeholder="Contoh: Gedung A, Ruang 1, Lantai 3" className="mb-5"></Input>
-                <Label className="mx-2 mb-1.5 text-base ">Deskripsi Barang</Label>
-                <Textarea value={deskripsi} onChange={(e) => setDeskripsi(e.target.value)} maxLength={250} placeholder="Masukkan Deskripsi Barang" className="mb-5" />
+                <Input
+                  value={namaBarang}
+                  onChange={(e) => setNamaBarang(e.target.value)}
+                  maxLength={20}
+                  placeholder="Masukkan Nama Barang"
+                  className="mb-5"
+                ></Input>
+                <Label className="mx-2 mb-1.5 text-base ">
+                  Lokasi Barang Ditemukan
+                </Label>
+                <Input
+                  value={lokasiTemu}
+                  onChange={(e) => setLokasiTemu(e.target.value)}
+                  maxLength={50}
+                  placeholder="Contoh: Gedung A, Ruang 1, Lantai 3"
+                  className="mb-5"
+                ></Input>
+                <Label className="mx-2 mb-1.5 text-base ">
+                  Deskripsi Barang
+                </Label>
+                <Textarea
+                  value={deskripsi}
+                  onChange={(e) => setDeskripsi(e.target.value)}
+                  maxLength={250}
+                  placeholder="Masukkan Deskripsi Barang"
+                  className="mb-5"
+                />
                 <section className="flex flex-col gap-4 mb-5">
                   <section>
-                    <Label htmlFor="date-picker" className="mx-2 mb-1.5 text-base">
+                    <Label
+                      htmlFor="date-picker"
+                      className="mx-2 mb-1.5 text-base"
+                    >
                       Tanggal
                     </Label>
                     <Popover open={open} onOpenChange={setOpen}>
                       <PopoverTrigger asChild>
-                        <Button variant="outline" id="date-picker" className="w-32 justify-between font-normal">
+                        <Button
+                          variant="outline"
+                          id="date-picker"
+                          className="w-32 justify-between font-normal"
+                        >
                           {date ? date.toLocaleDateString() : "Pilih tanggal"}
                           <ChevronDownIcon />
                         </Button>
                       </PopoverTrigger>
-                      <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+                      <PopoverContent
+                        className="w-auto overflow-hidden p-0"
+                        align="start"
+                      >
                         <Calendar
-                          disabled={{after: new Date()}}
+                          disabled={{ after: new Date() }}
                           mode="single"
                           selected={date}
                           captionLayout="dropdown"
@@ -136,7 +199,13 @@ export default function LaporanBarangTemu() {
                     <Label htmlFor="time-picker" className="px-1">
                       Waktu
                     </Label>
-                    <Input type="time" id="time-picker" step="60" value={time} onChange={(e)=> setTime(e.target.value)} />
+                    <Input
+                      type="time"
+                      id="time-picker"
+                      step="60"
+                      value={time}
+                      onChange={(e) => setTime(e.target.value)}
+                    />
                   </section>
                 </section>
                 <section className=" flex justify-end gap-5">
