f5d3bbbabe7ad9d1b9101ef76cc0411b7d4169c3
Deni Himawan <denihimawan595@gmail.com>
2026-01-22 22:03:58 +0700

    style: format code

--- web/src/hooks/useFoundReport.ts
+++ web/src/hooks/useFoundReport.ts
@@ -2,15 +2,12 @@ import useSWR from "swr";
 import { fetcher } from "./usefetch";
 
 export function useFoundReports() {
-  const { data, error, isLoading, mutate } = useSWR(
-    "/foundreport",
-    fetcher
-  );
+  const { data, error, isLoading, mutate } = useSWR("/foundreport", fetcher);
 
   return {
     data: data?.data,
     error,
     isLoading,
-    mutate
+    mutate,
   };
 }
