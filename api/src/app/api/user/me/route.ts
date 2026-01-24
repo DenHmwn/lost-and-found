f80b0535b7ca5e90f55bc07ab2514f77f38382f5
Deni Himawan <denihimawan595@gmail.com>
2026-01-24 16:01:37 +0700

    refactor: minor update

--- api/src/app/api/user/me/route.ts
+++ api/src/app/api/user/me/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
     // ambil id admin dari helper
     const users = await getAuth();
