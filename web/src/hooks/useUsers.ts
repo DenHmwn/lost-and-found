5decd82c4c01590246bc8645c3a49240bd9a688a
Deni Himawan <denihimawan595@gmail.com>
2026-01-26 15:18:54 +0700

    feat: update fetch data

--- web/src/hooks/useUsers.ts
+++ web/src/hooks/useUsers.ts
@@ -1,12 +1,17 @@
 import useSWR from "swr";
 import { fetcher } from "./useFetch";
 
-export function useUsers() {
-  const { data, error, isLoading } = useSWR("/user", fetcher);
+export function useUsers(page = 1, limit = 10) {
+  const { data, error, isLoading, mutate } = useSWR(
+    `/user?page=${page}&limit=${limit}`,
+    fetcher,
+  );
 
   return {
     data: data?.data,
+    pagination: data?.pagination,
     error,
     isLoading,
+    mutate
   };
 }
