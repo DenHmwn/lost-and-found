fcd675ea32401d9514461a1aee3763319f7cdda9
Deni Himawan <denihimawan595@gmail.com>
2026-01-26 15:18:09 +0700

    feat: update fetch data

--- web/src/hooks/useFoundReport.ts
+++ web/src/hooks/useFoundReport.ts
@@ -1,11 +1,15 @@
 import useSWR from "swr";
 import { fetcher } from "./useFetch";
 
-export function useFoundReports() {
-  const { data, error, isLoading, mutate } = useSWR("/foundreport", fetcher);
+export function useFoundReports(page = 1, limit = 10) {
+  const { data, error, isLoading, mutate } = useSWR(
+    `/foundreport?page=${page}&limit=${limit}`,
+    fetcher,
+  );
 
   return {
     data: data?.data,
+    pagination: data?.pagination,
     error,
     isLoading,
     mutate,
