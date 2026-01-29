3e0b70b66f2b9b10bc494a08c773dfa47cc67b55
Deni Himawan <denihimawan595@gmail.com>
2026-01-29 17:50:16 +0700

    refactor: minor update

--- web/src/hooks/useLostReport.ts
+++ web/src/hooks/useLostReport.ts
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import { fetcher } from "./useFetch";
+import { fetcher } from "../services/fetchService";
 
 export function useLostReports(page = 1, limit = 10) {
   const { data, error, isLoading, mutate } = useSWR(
